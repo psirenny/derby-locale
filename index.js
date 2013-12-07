b5754402e9f9b2c059b7824c2aa91ec61f15f37c
psirenny <djtorres0@gmail.com>
2013-12-07 15:00:33 -0500

    remove app

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-exports.app = require('./lib/app');
-exports.server = require('./lib/server');
+var _ = require('lodash')
+  , locale = require('locale');
+
+module.exports = function (app, options) {
+  if (!options) options = {};
+
+  return function (req, res, next) {
+    var model = req.getModel()
+      , preferred = model.setNull('$locale.preferred', options.preferred)
+      , supported = model.setNull('$locale.supported', options.supported);
+
+    preferred = _(preferred).flatten().compact().unique().value();
+    model.set('$locale.preferred', preferred);
+    preferred = new locale.Locales(preferred);
+    model.set('$locale.locale', preferred.best(supported).toString());
+    next();
+  };
+};
