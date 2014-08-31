ad603cc5aff24f0ec25cb78912353ebf3dc616a2
psirenny <djtorres0@gmail.com>
2014-08-31 08:03:15 -0400

    refactor into app/server, add tests

---
+++ test/index.js
@@ -0,0 +1,91 @@
+var lib = require('..');
+var should = require('chai').should();
+
+describe('derby-locale', function () {
+  it('should be an object', function () {
+    lib.should.be.an('object');
+  });
+
+  describe('app', function () {
+    it('should be an object', function () {
+      lib.app.should.be.an('object');
+    });
+
+    describe('locale', function () {
+      it('should return a blank locale', function () {
+        lib.server.load(function (err, $locale) {
+          var locale = lib.app.locale($locale);
+          locale.should.be.a('string');
+          locale.should.be.blank;
+        });
+      });
+
+      it('should return the first supported locale', function () {
+        var opts = {supported: ['es', 'ca']};
+        lib.server.load(opts, function (err, $locale) {
+          var locale = lib.app.locale($locale);
+          locale.should.be.a('string');
+          locale.should.eql('es');
+        });
+      });
+
+      it('should return the best locale', function () {
+        var opts = {supported: ['es', 'en_US']};
+        opts.strategies = {test: {preferred: ['en', 'es']}};
+        lib.server.load(opts, function (err, $locale) {
+          var locale = lib.app.locale($locale);
+          locale.should.be.a('string');
+          locale.should.eql('en_US');
+        });
+      });
+
+      it('should order strategies by order num', function () {
+        var opts = {supported: ['en', 'es', 'de']};
+        opts.strategies = {};
+        opts.strategies.test1 = {order: 2, preferred: ['es', 'en']};
+        opts.strategies.test2 = {order: 1, preferred: ['de']};
+        lib.server.load(opts, function (err, $locale) {
+          var locale = lib.app.locale($locale);
+          locale.should.be.a('string');
+          locale.should.eql('de');
+        });
+      });
+    });
+  });
+
+  describe('server', function () {
+    it('should be a function', function () {
+      lib.server.should.be.a('function');
+    });
+
+    describe('load', function () {
+      it('should be a function', function () {
+        lib.server.load.should.be.a('function');
+      });
+
+      it('should return a $locale object', function () {
+        lib.server.load(function (err, $locale) {
+          (err === null).should.be.true;
+          $locale.should.be.an('object');
+          $locale.should.have.property('strategies');
+          $locale.strategies.should.be.an('object');
+          $locale.should.have.property('supported');
+          $locale.supported.should.be.an('array');
+        });
+      });
+
+      it('should accept options', function () {
+        var opts = {supported: ['en', 'es']};
+        lib.server.load(opts, function (err, $locale) {
+          (err === null).should.be.true;
+          $locale.should.be.an('object');
+          $locale.should.have.property('strategies');
+          $locale.strategies.should.be.an('object');
+          $locale.should.have.property('supported');
+          $locale.supported.should.be.an('array');
+          $locale.supported.should.eql(opts.supported);
+        });
+      });
+    });
+  });
+});
